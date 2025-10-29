fd7eddbaec72eb11d51dfd5d5dbd148269e01e18
Faith Alonge <126901421+sloetag@users.noreply.github.com>
2025-10-29 18:56:23 +0100

    Add files via upload

--- src/components/pages/Home/OurStory.js
+++ src/components/pages/Home/OurStory.js
@@ -8,8 +8,7 @@ const OurStory = () => {
       <div className="our-story-description">
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
-        <p>
-          **Our Story**  
+        <p> 
 
 Little Lemon began as a dream shared by two cousins, Mario and Adrian, who grew up cooking together in their grandmother’s sun-drenched kitchen on the Mediterranean coast. Inspired by her simple, vibrant recipes—bursting with lemon, olive oil, fresh herbs, and love—they brought those flavors to the city. What started as a tiny corner bistro has blossomed into a beloved neighborhood gem, where every dish honors tradition while embracing fresh, local ingredients. At Little Lemon, we don’t just serve food—we share memories, warmth, and the timeless joy of gathering around the table. Come taste the Mediterranean, made with heart.
         </p>
